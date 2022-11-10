af5182532da0ca98c21eb72bf898cc58fc53b9da
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2022-11-10 12:50:53 +0100

    feat: website setup

---
+++ src/App/Logo.tsx
@@ -0,0 +1,5 @@
+import onlyDustLogo from "assets/img/onlydust-logo.png";
+
+export default function Logo() {
+  return <img className="md:w-64 w-48 hover:opacity-90" src={onlyDustLogo} alt="OnlyDust Logo" />;
+}
