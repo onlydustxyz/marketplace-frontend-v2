1520ef8e86161242e1a3230f4adb4d963fdecdc9
nicolas.ngomai <nicolasn@theodo.fr>
2022-11-22 19:09:25 +0100

    fix: fix illegal invocation error

--- src/infra/localStorage.tsx
+++ src/infra/localStorage.tsx
@@ -1,2 +1,2 @@
-export const getItem = localStorage.getItem;
-export const setItem = localStorage.setItem;
+export const getItem = (key: string) => localStorage.getItem(key);
+export const setItem = (key: string, value: string) => localStorage.setItem(key, value);
