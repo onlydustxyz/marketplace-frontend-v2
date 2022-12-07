b1332d1cf15b0e6ebe0f72933c90afcd05043167
Stanislas Bernard <bernardstanislas@gmail.com>
2022-12-07 13:16:17 +0100

    chore: fix storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,13 +2,23 @@ import "tailwindcss/tailwind.css";
 import "src/assets/css/index.css";
 import "src/assets/fonts/Alfreda/stylesheet.css";
 import "src/assets/fonts/GTWalsheimPro/stylesheet.css";
+import { IntlProvider } from "src/hooks/useIntl";
+import React from "react";
 
 export const parameters = {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  Story => (
+    <IntlProvider>
+      <Story />
+    </IntlProvider>
+  ),
+];
